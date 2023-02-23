b84b3ba9f47e6a3054981b85472a81ad0e4e340e
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2023-02-23 21:50:35 +0400

    chore: 🔧 Changes that do not modify src or test files

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@
 # Makefile readme (en): <https://www.gnu.org/software/make/manual/html_node/index.html#SEC_Contents>
 
 SHELL = /bin/sh
-LDFLAGS = "-s -w -X github.com/tarampampam/error-pages/internal/version.version=$(shell git rev-parse HEAD)"
+LDFLAGS = "-s -w -X gh.tarampamp.am/error-pages/internal/version.version=$(shell git rev-parse HEAD)"
 
 DC_RUN_ARGS = --rm --user "$(shell id -u):$(shell id -g)"
 APP_NAME = $(notdir $(CURDIR))
--- cmd/error-pages/main.go
+++ cmd/error-pages/main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	crypto "crypto/rand"
-	"encoding/binary"
-	"math/rand"
 	"os"
 	"path/filepath"
-	"time"
 
 	"github.com/fatih/color"
 	"go.uber.org/automaxprocs/maxprocs"
 
-	"github.com/tarampampam/error-pages/internal/cli"
+	"gh.tarampamp.am/error-pages/internal/cli"
 )
 
 // set GOMAXPROCS to match Linux container CPU quota.
@@ -26,15 +22,6 @@ func main() { exitFn(run()) }
 // run this CLI application.
 // Exit codes documentation: <https://tldp.org/LDP/abs/html/exitcodes.html>
 func run() int {
-	var b [8]byte
-
-	// seed random number generator
-	if _, err := crypto.Read(b[:]); err == nil {
-		rand.Seed(int64(binary.LittleEndian.Uint64(b[:]))) // https://stackoverflow.com/a/54491783/2252921
-	} else {
-		rand.Seed(time.Now().UnixNano()) // fallback
-	}
-
 	if err := (cli.NewApp(filepath.Base(os.Args[0]))).Run(os.Args); err != nil {
 		_, _ = color.New(color.FgHiRed, color.Bold).Fprintln(os.Stderr, err.Error())
 
